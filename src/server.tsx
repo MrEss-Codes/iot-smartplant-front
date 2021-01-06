1a9c76fff65e7d63bf99017d5fcb533932e68e4b
Emil <emildk11@live.dk>
2021-01-06 22:41:52 +0100

    heroku port

--- src/server.tsx
+++ src/server.tsx
@@ -4,7 +4,7 @@ const next = require('next')
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 app.prepare()
     .then(() => {
